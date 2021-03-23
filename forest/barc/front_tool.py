80fe1861c9f065553a4d8076b1d9d3b85458b05e
Dan Walker <daniel.walker@ncas.ac.uk>
2021-03-23 13:55:11 +0000

    Spelling of Prof. Bézier's name

--- forest/barc/front_tool.py
+++ forest/barc/front_tool.py
@@ -10,7 +10,7 @@ from bokeh.models import EditTool, Drag, Tap, CustomJS
 
 class FrontDrawTool(EditTool, Drag, Tap):
     '''
-    A tool for drawing Beziér curves with Unicode text evenly-spaced along it (e.g. for 
+    A tool for drawing Bézier curves with Unicode text evenly-spaced along it (e.g. for 
     weather fronts)
     '''
     __css__ = "forest/static/style_barc.css?v=27"
