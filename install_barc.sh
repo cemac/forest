2cfcc116c91853c835f141ca8efb86704c260023
Helen Burns <H.L.Burns@leeds.ac.uk>
2021-04-12 16:20:04 +0100

    :wrench: :bulb: noticed missing installation line

--- install_barc.sh
+++ install_barc.sh
@@ -9,9 +9,14 @@
 #bash_version   :4.2.46(2)-release
 #============================================================================
 echo "downloading submodules"
+# install submodules
 git submodule init
 git submodule update --init --recursive
+# create blank database file
 cp -p forest/barc/barc-save_template.sdb forest/barc/barc-save.sdb 
+# create and activate virtual environment
 conda create --name forest-barc
+conda activate forest-barc
 conda install -c conda-forge --file requirements.txt --file requirements-dev.txt -y
 python setup.py develop
+echo "set up complete"
